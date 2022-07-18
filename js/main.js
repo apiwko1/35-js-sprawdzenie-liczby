9c70bf426a4199392b4789701b80a6624fa8a3d9
apiwko1 <70264036+apiwko1@users.noreply.github.com>
2022-07-18 16:51:15 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function spr_zakres(liczba){
+    if((liczba >= 100) && (liczba <= 200)){
+        console.log('Liczba znajduje się w przedziale');
+    }else{
+        console.log('Liczba nie znajduje się w przedziale');
+    }
+}
+spr_zakres(150);
